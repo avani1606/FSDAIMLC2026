9a80c9ee9747245b5181942998b59b54044bbda3
Avani Goel <avanigoel2006@gmail.com>
2026-07-31 10:26:43 +0530

    msg

--- Index.js
+++ Index.js
@@ -40,10 +40,107 @@
 // console.log("Square root of 25 is "+result); u can use this method also to return the value and store it in a variable and then print it.
 
 //IIFE (Immediate Invoked Function Expression)
-// (()=>{console.log("HELLOoooooooooooooooo IIFE")})();
+// (()=>{console.log("HELLOoooooooooooooooo IIFE")})(
 
-function sum(a,b){
-    return a+b;
+// function sum(a,b){
+//     return a+b;
+// }
+// const result=sum(10,20);
+// console.log("sum " +result);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// function sumwithMsg(clbk,msg){
+//     const result=clbk(20,40);
+//     console.log(msg+":" +result);
+// }
+// sumwithMsg(sum,"Hey using calculation with js")
+
+
+// function login(msg,error){
+//     if(error){
+//         console.log("Error is" +error);
+//     }
+//     else{
+//         console.log("Welcome "+msg);
+//     }
+// }
+
+// function loginVerification(username,password,clbk){
+// if(username=="Alice" && password=="12345"){
+//     clbk("Alice",null);
+// }else{
+//     clbk(null,"Invalid username or password");
+// }
+// }
+// loginVerification("Alice","12345",login);
+
+// function randomnumberRepeated(num, clbk){
+//    let randomNum=Math.floor(Math.random()*10);
+//    clbk(randomNum);
+// }
+// randomnumberRepeated(10,(result)=>console.log("Random Number:", result));
+
+// console.log("First")
+// setTimeout(()=>(console.log("Second")),1000)
+
+// for(i=0;i<1000;i++){
+//     console.log("Second")
+// }
+// console.log("Third")
+
+//callback hell
+// setTimeout(()=>{
+//     setTimeout(()=>{
+//         setTimeout(()=>{
+//             setTimeout(()=>{
+//                 setTimeout(()=>{
+//                     setTimeout(()=>{
+//                         setTimeout(()=>{
+//                             setTimeout(()=>{
+//                                 setTimeout(()=>{
+//                                     setTimeout(()=>{
+//                                         console.log("Hello from another world")
+//                                     },1000)
+//                                 },1000)
+//                             },1000)
+//                         },1000)
+//                     },1000)
+//                 },1000)
+//             },1000)
+//         },1000)
+//     },1000)
+// },1000)
+
+// Promises
+
+// mypromise = new Promise((resolve,reject)=>{
+//     username="admin";
+//     password="12345";
+//     if(username=="admin" && password=="12345"){
+//         resolve("Login successful");
+//     }else{
+//         reject("Login Failed");
+//     }
+// })
+
+// mypromise.then((msg)=>{
+//     console.log(msg)
+// }).catch((msg)=>{
+//     console.log(msg)
+// }).finally(()=>{
+//     console.log("All the resources have been closed")
+// })
+
+async function handlelog(){
+    try{
+        await myPromise
+    } catch(error){
+        console.log(error);
+    }
+    finally{
+        console.log("Login execution successful");
+    }
 }
-const result=sum(10,20);
-console.log("sum " +result);
+handlelog();
