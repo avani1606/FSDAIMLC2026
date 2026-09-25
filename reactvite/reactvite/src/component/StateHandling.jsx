e8dd1438252afc9fbd6705f3e3569666dc93b9ae
Avani Goel <avanigoel2006@gmail.com>
2026-09-25 10:10:43 +0530

    Work till 25/9/26

--- reactvite/reactvite/src/component/StateHandling.jsx
+++ reactvite/reactvite/src/component/StateHandling.jsx
@@ -1,17 +1,27 @@
 import React, {useState} from "react";
 
 function StateHandling() {
-  const [counter, setCounter] = useState(29);
+    const[counter,setCounter]=useState(20);
+    const[red,setRed]=useState(0);
+    const[green,setGreen]=useState(0);
+    const[blue,setBlue]=useState(0);
 function increment(){
     setCounter(counter+10);
 }
+function changeBGColor(){
+    setRed(Math.random()*255);
+    setGreen(Math.random()*255);
+    setBlue(Math.random()*255);
+}
   return (
-    <div>Counter App
+    <div style={{backgroundColor:`rgb(${red},${green},${blue})`,border:'2px solid red', height:'300px', width:'300px'}}>
+<h2 style={{color:'brown'}}> Counter App</h2>
       <h2>Counter={counter}</h2>
       <button onClick={increment}>increaseCounter</button>
       <button onClick={()=>setCounter(counter-5)}>decreaseCounter</button>
+      <button onClick={changeBGColor}>Change Background Color</button>
     </div>
-  );
+  )
 
 }
 
